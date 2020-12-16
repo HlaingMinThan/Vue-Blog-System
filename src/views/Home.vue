1e66252529e39d6c331801c03ec8a568337b1faa
HlaingMinThan <hlaingminthan92@gmail.com>
2020-12-16 22:06:12 +0630

    fetching posts data with aync and await function in composition api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
-     
-         <PostsList :posts="posts"></PostsList>
+          <div v-if="error">
+              {{error}}
+          </div>
+          <div v-if="posts.length>0">
+             <PostsList :posts="posts"></PostsList>
+          </div>
+          <div v-else>
+              loading...
+          </div>
        
     
   </div>
@@ -15,10 +22,25 @@ export default {
   components: { PostsList },
     setup(){
     
-      let posts=ref([
-      
-      ])
-      return {posts};
+      let posts=ref([]);
+      let error=ref("");
+      let load=async()=>{
+         try{
+           let response= await fetch("http://localhost:3000/posts")
+           if(response.status===404){
+             throw new Error("not found url");
+           }
+            let datas=await response.json()
+            posts.value=datas
+         }catch(err){
+            // console.log(error.message)
+            error.value=err.message;
+         }
+      }
+
+      load();
+
+      return {posts,error};
     }
 }
 </script>
