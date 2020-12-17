9e6de9b64fa9a339d184805706adb89bf9e4811d
HlaingMinThan <hlaingminthan92@gmail.com>
2020-12-17 23:44:55 +0630

    create post backend

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,8 +1,13 @@
 <template>
-    <router-link :to="{name:'Detail',params:{id:post.id}}">
-        <h2>{{post.title}}</h2>
-    </router-link>
-    <p>{{cutPostBody}}</p>
+   <div class="post">
+        <router-link :to="{name:'Detail',params:{id:post.id}}">
+            <h2>{{post.title}}</h2>
+        </router-link>
+        <p>{{cutPostBody}}</p>
+        <div v-for="tag in post.tags" :key="tag" class="pill">
+          {{tag}}
+        </div>
+   </div>
 </template>
 
 <script>
@@ -19,5 +24,38 @@ export default {
 </script>
 
 <style>
-
+    .post {
+        margin: 0 40px 30px;
+        padding-bottom: 30px;
+        border-bottom: 1px dashed #e7e7e7;
+      }
+      .post h2 {
+        display: inline-block;
+        position: relative;
+        font-size: 26px;
+        color: white;
+        margin-bottom: 10px;
+        max-width: 400px;
+      }
+      .post h2::before {
+        content: "";
+        display: block;
+        width: 100%;
+        height: 100%;
+        background: #ff8800;
+        position: absolute;
+        z-index: -1;
+        padding-right: 40px;
+        left: -30px;
+        transform: rotateZ(-1deg);
+      }
+      .pill {
+        display: inline-block;
+        margin: 10px 10px 0 0;
+        color: #444;
+        background: #ddd;
+        padding: 8px;
+        border-radius: 20px;
+        font-size: 14px;
+      }
 </style>
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent="addPost">
     <label>Title</label>
     <input type="text" required v-model="title">
 
@@ -29,7 +29,22 @@ export default {
       }
       tag.value=""
     }
-    return {title,body,tag,handleKeydown,tags}
+    let addPost=async()=>{
+        await fetch("http://localhost:3000/posts",{
+          method:"POST",
+          headers:{
+            "Content-type":"application/json"
+          },
+          body:JSON.stringify(
+            {
+              title:title.value,//""
+              body:body.value,//""
+              tags:tags.value//[]
+            }
+          )
+        })
+    }
+    return {title,body,tag,handleKeydown,tags,addPost}
   }
 }
 </script>
