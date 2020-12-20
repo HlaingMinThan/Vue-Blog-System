0f62b6d1acc92b7ff67bc8822c4a97316f1f8f32
HlaingMinThan <hlaingminthan92@gmail.com>
2020-12-20 23:44:22 +0630

    fetch our firebase posts collection documents

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,19 +1,16 @@
 import { ref } from "vue";
+import {db} from "../firebase/config"
 let getPosts=()=>{
     let posts=ref([]);
       let error=ref("");
       let load=async()=>{
          try{
-            // await new Promise((resolve,reject)=>{
-            //    // resolve();
-            //    setTimeout(resolve,2000)
-            // })
-           let response= await fetch("http://localhost:3000/posts")
-           if(response.status===404){
-             throw new Error("not found url");
-           }
-            let datas=await response.json()
-            posts.value=datas
+           let res=await db.collection("posts").get()
+               posts.value=res.docs.map((doc)=>{
+                  return {id:doc.id,...doc.data()}
+                  // console.log(doc.id);
+               })
+               // console.log(res.docs)               
          }catch(err){
             // console.log(error.message)
             error.value=err.message;
